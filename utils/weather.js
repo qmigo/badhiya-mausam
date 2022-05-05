8dcbcf31ca2ca1e996ea89cfe8a9d1f47680b142
qmigo <ankur031100@gmail.com>
2022-05-06 00:34:06 +0530

    first commit everything working

---
+++ utils/weather.js
@@ -0,0 +1,79 @@
+let address = {
+    api_key : "b8578d4c622040c9853e71f21c260f62",
+    fetchLocation : function(){
+        document.querySelector('input').placeholder='Locating your city ...'
+        let url =`https://ipgeolocation.abstractapi.com/v1/?api_key=b8578d4c622040c9853e71f21c260f62`
+        fetch(url)
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log(data);
+            let {city} = data
+            let {country} = data
+            if (city)
+            weather.fetchWeather(city)
+            else 
+            weather.fetchWeather(country)
+        })
+        .catch((err)=>{
+            weather.fetchWeather('Mumbai')
+        })
+    }
+}
+let weather = {
+    api_key : "09bf15dcf2b914980476116f8f3aa54a",
+    fetchWeather : function (city){
+        let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.api_key}&units=metric`
+        document.querySelector('input').placeholder='Loading ...'
+        fetch(url)
+        .then((response)=>response.json())
+        .then((data)=>this.displayWeather(data))
+        .catch((err)=>{
+            document.querySelector('.details').innerHTML=`
+            <p>Sorry City not found</p>
+        `
+        document.querySelector('input').placeholder='Enter a city ...'
+        })
+    },
+    displayWeather : function (data){
+        let {name, base} = data
+        let {temp, temp_min, temp_max} = data.main
+        let {description, icon} = data.weather[0]
+        let {speed} = data.wind
+
+        document.querySelector('.details').innerHTML=`
+        <p>Name : ${name}</p>
+        <p>Temp : ${temp} &deg;</p>
+        <img id="icon" src="https://openweathermap.org/img/wn/${icon}.png"></img>
+        <p>Wind Speed : ${speed} m/s</p>
+        <p>Description : ${description}</p>
+        `
+        document.querySelector('input').placeholder='Enter a city ...'
+
+        // console.log(name,temp,temp_min,temp_max,description,icon,base);
+    },
+    search : function(){
+        let input = document.getElementById('input').value;
+        if(input.search(/[^a-z]/i)!=-1 || input.length>20 || input.length==0)
+        {
+            document.getElementById('input').value=""
+            document.querySelector('input').placeholder='You made a typo !!!'
+        }
+        else 
+        {
+        this.fetchWeather(input)
+        document.getElementById('input').value=""
+        }
+    }
+}
+
+
+document.querySelector('.search').addEventListener('click',()=>{
+    weather.search()
+})
+
+document.querySelector('#input').addEventListener('keyup',(e)=>{
+    if(e.key=="Enter")
+    weather.search()
+})
+
+address.fetchLocation()
